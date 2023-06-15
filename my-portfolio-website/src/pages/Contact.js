076ee906ebd164fce348b2ac95d2b8f5e5274c6d
Areeb Karim <areebakarim99@gmail.com>
2023-06-15 13:03:24 -0400

    done

--- my-portfolio-website/src/pages/Contact.js
+++ my-portfolio-website/src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Contact () {
     return (
-        <div>
+        <div style={{color:"black"}}>
             Hi
         </div>
     )
--- my-portfolio-website/src/pages/Home.js
+++ my-portfolio-website/src/pages/Home.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 function Home () {
     return (
-        <div>
+        <div style={{color:"black"}}>
             Hi
+            main
+            main
+            main
+            main
+            main
+            main
+            main
+            main
+            main
+            main
+            main
+            main
+            main
+            main
+            main
+            main
+            
         </div>
     )
 }
